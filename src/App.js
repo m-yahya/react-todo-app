91525c9b9a070baead9dd4828bcd79f00b8048ae
m-yahya <yahya_gis@hotmail.com>
2022-06-02 14:10:10 +0200

    create a new todo item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [todo, setTodo] = useState("");
+
   return (
     <div className="App">
       <h1>React Todo App</h1>
 
       <div className="input-wrapper">
-        <input type="text" name="todo" placeholder="Create a new todo" />
+        <input
+          type="text"
+          name="todo"
+          value={todo}
+          placeholder="Create a new todo"
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
         <button className="add-button">Add</button>
       </div>
     </div>
