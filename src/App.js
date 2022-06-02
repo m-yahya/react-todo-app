353f17c0f80cf93e6d714eb323df6c7042ddca07
m-yahya <yahya_gis@hotmail.com>
2022-06-02 16:39:06 +0200

    conditionally display todo items

--- src/App.js
+++ src/App.js
@@ -30,6 +30,22 @@ const App = () => {
           Add
         </button>
       </div>
+
+      {todos?.length > 0 ? (
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <div className="todo">
+              <li key={index}> {todo} </li>
+
+              <button className="delete-button">Delete</button>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">
+          <p>No task found</p>
+        </div>
+      )}
     </div>
   );
 };
