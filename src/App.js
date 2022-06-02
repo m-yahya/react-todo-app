98e8441b22fe934398a19798a7fc02b73d2c492a
m-yahya <yahya_gis@hotmail.com>
2022-06-02 12:03:04 +0200

    create initial mockup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ const App = () => {
   return (
     <div className="App">
       <h1>React Todo App</h1>
+
+      <div className="input-wrapper">
+        <input type="text" name="todo" placeholder="Create a new todo" />
+        <button className="add-button">Add</button>
+      </div>
     </div>
   );
 };
